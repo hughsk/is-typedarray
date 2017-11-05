fcab360702c974e011604ffb100762efccf2fe62
Bharath <bharathvaj1995@gmail.com>
2017-11-05 22:46:52 +0530

    Add more specific APIs and code clean up

--- index.js
+++ index.js
@@ -1,41 +1,57 @@
-module.exports      = isTypedArray
-isTypedArray.strict = isStrictTypedArray
-isTypedArray.loose  = isLooseTypedArray
+'use strict';
+const toString = Object.prototype.toString;
+const getObjectType = arr => toString.call(arr).slice(8, -1);
+const isObjectOfType = type => arr => getObjectType(arr) === type;
 
-var toString = Object.prototype.toString
-var names = {
-    '[object Int8Array]': true
-  , '[object Int16Array]': true
-  , '[object Int32Array]': true
-  , '[object Uint8Array]': true
-  , '[object Uint8ClampedArray]': true
-  , '[object Uint16Array]': true
-  , '[object Uint32Array]': true
-  , '[object Float32Array]': true
-  , '[object Float64Array]': true
-}
+const typedArrayTypes = new Set([
+  'Int8Array',
+  'Uint8Array',
+  'Uint8ClampedArray',
+  'Int16Array',
+  'Uint16Array',
+  'Int32Array',
+  'Uint32Array',
+  'Float32Array',
+  'Float64Array'
+]);
 
-function isTypedArray(arr) {
+// Legacy APIs
+const isLooseTypedArray = arr => {
+  return typedArrayTypes.has(getObjectType(arr));
+};
+
+const isStrictTypedArray = arr => {
   return (
-       isStrictTypedArray(arr)
-    || isLooseTypedArray(arr)
-  )
-}
+    arr instanceof Int8Array ||
+    arr instanceof Int16Array ||
+    arr instanceof Int32Array ||
+    arr instanceof Uint8Array ||
+    arr instanceof Uint8ClampedArray ||
+    arr instanceof Uint16Array ||
+    arr instanceof Uint32Array ||
+    arr instanceof Float32Array ||
+    arr instanceof Float64Array
+  );
+};
 
-function isStrictTypedArray(arr) {
-  return (
-       arr instanceof Int8Array
-    || arr instanceof Int16Array
-    || arr instanceof Int32Array
-    || arr instanceof Uint8Array
-    || arr instanceof Uint8ClampedArray
-    || arr instanceof Uint16Array
-    || arr instanceof Uint32Array
-    || arr instanceof Float32Array
-    || arr instanceof Float64Array
-  )
-}
+const is = arr => {
+  if (isStrictTypedArray(arr) || isLooseTypedArray(arr)) {
+    return true;
+  } else {
+    return false;
+  }
+};
 
-function isLooseTypedArray(arr) {
-  return names[toString.call(arr)]
-}
+is.loose = isLooseTypedArray;
+is.strict = isStrictTypedArray;
+is.int8Array = isObjectOfType('Int8Array');
+is.uint8Array = isObjectOfType('Uint8Array');
+is.uint8ClampedArray = isObjectOfType('Uint8ClampedArray');
+is.int16Array = isObjectOfType('Int16Array');
+is.uint16Array = isObjectOfType('Uint16Array');
+is.int32Array = isObjectOfType('Int32Array');
+is.uint32Array = isObjectOfType('Uint32Array');
+is.float32Array = isObjectOfType('Float32Array');
+is.float64Array = isObjectOfType('Float64Array');
+
+module.exports = is;
